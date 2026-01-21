328fed3ed28f4982ad74b40fa59c851fc5c5fac7
James Krueger <jkrueger@ps-engineering.com>
2026-01-21 15:15:16 -0500

    Initial commit of all backend code in a separate branch

---
+++ backend/src/main/java/com/psedb/timeclock/TimeclockApplication.java
@@ -0,0 +1,13 @@
+package com.psedb.timeclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeclockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeclockApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/psedb/timeclock/TimeclockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psedb.timeclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeclockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
